0e74fe1cce2a955ee9139e8a6cfc3bd020c32b46
jaocosta <62314208+jaocosta@users.noreply.github.com>
2020-05-11 22:10:05 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -18,29 +18,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Custom Fonts',
-      // Set Raleway as the default app font.
-      theme: ThemeData(fontFamily: 'Raleway'),
-      home: MyHomePage(),
-    );
-  }
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      // The AppBar uses the app-default Raleway font.
-      appBar: AppBar(title: Text('Custom Fonts')),
-      body: Center(
-        // This Text widget uses the RobotoMono font.
-        child: Text(
-          'Roboto Mono sample',
-          style: TextStyle(fontFamily: 'RobotoMono'),
-        ),
-      ),
-    );
-  }
-}
